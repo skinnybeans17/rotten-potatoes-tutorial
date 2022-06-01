73341d57ddba0b6a9073c9dfa5f161cc3ccd9e81
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-06-01 11:05:18 -0700

    single

--- app.js
+++ app.js
@@ -12,11 +12,22 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
 //var reviews = [
 //    { title: "Great Review", movieTitle: "Batman II" },
 //    { title: "Awesome Movie", movieTitle: "Titanic" }
 //]
   
+const Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    movieTitle: String
+  });
+
 app.get('/', (req, res) => {
 res.render('reviews-index', { reviews: reviews });
 })
@@ -45,18 +56,20 @@ app.get('/reviews/new', (req, res) => {
 
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect('/');
+        console.log(review)
+        res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
     }).catch((err) => {
-        console.log(err.message);
+        console.log(err.message)
     })
 })
 
-const Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    movieTitle: String
-  });
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+      res.render('reviews-show', { review: review })
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
