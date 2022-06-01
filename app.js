e5819c50dfd46359349560b53f6efc9e0208d0e7
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-06-01 12:21:25 -0700

    edit

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const app = express()
+const methodOverride = require('method-override')
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const app = express()
 
 var exphbs = require('express-handlebars');
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 let reviews = [
     { title: "Great Review", movieTitle: "Batman II" },
@@ -51,8 +54,8 @@ Reviews.find()
 });
 
 app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
-  })
+    res.render('reviews-new', {title: "New Review"});
+})
 
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
@@ -63,13 +66,21 @@ app.post('/reviews', (req, res) => {
     })
 })
 
-app.get('/reviews/:id', (req, res) => {
-    Review.findById(req.params.id).then((review) => {
-      res.render('reviews-show', { review: review })
-    }).catch((err) => {
-      console.log(err.message);
-    })
+app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+      .then(review => {
+        res.redirect(`/reviews/${review._id}`)
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
   })
+
+app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id, function(err, review) {
+      res.render('reviews-edit', {review: review, title: "Edit Review"});
+    })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
